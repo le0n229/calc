52aa0b32aaddd3d91c3a18dbbd5ae584eb9a2cd5
le0n229 <leon229@yandex.ru>
2019-01-25 12:25:52 +0300

    buttons

--- logic.js
+++ logic.js
@@ -1,9 +1,49 @@
-function multiply(a,b) {
+var oneButton = document.getElementById('one')
+
+var addButton = document.getElementById('add')
+
+var equalButton = document.getElementById('equal')
+
+var action = ""
+var a = ''
+var b = ''
+var reset = true
+
+oneButton.addEventListener('click', function () {
+    if (reset === true) {
+        let dig = '1';
+        (document.getElementById('digits')).innerHTML = dig
+        reset = false
+    } else {
+        let dig = (document.getElementById('digits')).innerHTML
+        dig = dig + '1';
+        (document.getElementById('digits')).innerHTML = dig
+        reset = false
+    }
+})
+
+
+addButton.addEventListener('click', function () {
+    action = '+'
+    a = (document.getElementById('digits')).innerHTML
+    reset = true
+})
+
+equalButton.addEventListener('click', function () {
+    b = (document.getElementById('digits')).innerHTML
+    var dig = ''
+    dig = calc(a, action, b)
+        (document.getElementById('digits')).innerHTML = dig
+    reset = true
+})
+
+
+function multiply(a, b) {
     return a * b;
 }
 
 
-function divide(a,b) {
+function divide(a, b) {
     return a / b;
 }
 
@@ -20,23 +60,23 @@ function reset() {
 }
 
 function result() {
-      
+
 }
 
 function dot(a) {
     return a + '.';
 }
 
-/*(function calc(a, action, b) {
+function calc(a, action, b) {
     if (action === '-') {
-      a - b;
+        return a - b;
     } else if (action === '+') {
-      return a + b;
+        var c=''
+        c = a+b
+        return c;
     } else if (action === '*') {
-      return a * b;
+        return a * b;
     } else if (action === '/') {
-      return a / b;
+        return a / b;
     }
-  }
-  console.log(calc(5,'-',7));
-     */   
+}
