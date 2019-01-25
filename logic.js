bacaec219304ef4497b6ac2e17368f9931f92acb
Артемий Присяжнюк <macarty@Artemijs-MacBook-Pro.local>
2019-01-25 11:36:12 +0300

    js logic

---
+++ logic.js
@@ -0,0 +1,42 @@
+function multiply(a,b) {
+    return a * b;
+}
+
+
+function divide(a,b) {
+    return a / b;
+}
+
+function add(a, b) {
+    return a + b;
+}
+
+function sub(a, b) {
+    return a - b;
+}
+
+function reset() {
+    return 0;
+}
+
+function result() {
+      
+}
+
+function dot(a) {
+    return a + '.';
+}
+
+/*(function calc(a, action, b) {
+    if (action === '-') {
+      a - b;
+    } else if (action === '+') {
+      return a + b;
+    } else if (action === '*') {
+      return a * b;
+    } else if (action === '/') {
+      return a / b;
+    }
+  }
+  console.log(calc(5,'-',7));
+     */   
